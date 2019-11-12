cfbb4e4bbce3311ef30fa95395454a664671ed73
= <=>
2019-11-12 17:31:09 -0600

    Low changes

--- get.php
+++ get.php
@@ -1,1 +1,21 @@
-sa
+<?php 
+
+$matches = [];
+
+if(preg_match('/\/([^\/]+\/([^\/]+)/', $_SERVER["REQUEST_URI"], $matches))
+{
+	$_GET['resource_type'] = $matches[1];
+	$_GET['resource_id'] = $matches[2];
+
+	error_log(print_r($matches, 1));
+	require 'server.php';
+} elseif (preg_match('/\/([^\/]+)\/?/', $_SERVER["REQUEST_URI"], $matches))
+{
+	$_GET['resource_type'] = $matches[1];
+	error_log(print_r($matches, 1));
+}else
+{
+	error_log('No matches');
+	http_response_code(404);
+}
+
--- index.php
+++ index.php
@@ -1,8 +1,46 @@
 <?php
+	
+	//Se definen los recursos disponibles
+	$allowedResourceTypes = [
+		'tabla', 
+		'tabla2'
+	];
 
-	class conexion()
+	//Obtiene el tipo de recurso
+	$resourceType = $_GET['resorce_type'];
+
+	//Valida que el recurso sea valido
+	if(!in_array($resorceType, $allowedResourceTypes)) {die;}
+
+
+	header('Conyent-Type: application/json');
+	
+	$resourceId = array_key_exists('resource_id', $_GET) ? $_GET['resource_id'] : '';
+
+	//Se procesa la respuesta
+	switch(strtoupper($_SERVER['REQUEST_METHOD']))
 	{
-		conn=>
+		case 'GET':
+			break;
+		case 'POST':
+			$json = file_get_contents('php://input');
+			$books[] = json_decode($json, true);
+
+			echo array_keys( $books )[ count($books) -1];
+			break;
+		case 'PUT':
+			if(!empty($resourceId) && array_key_exists($resourceId, $books))
+			{
+				$json = file_get_contents('php://input');
+				$books[ $resourceId ] = json_decode($json, true);
+			}
+			break;
+		case 'DELETE':
+			if(!empty($resourceId) && array_key_exists($resourceId, $books))
+			{
+				unset($books[ $resourceId]);
+			}
+			break;
 
 	}
 ?>
